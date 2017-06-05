67d6bacd1ced0108b2198e490a55d0a12a619e13
Larry Stuart <lstuart@equitable.ca>
2017-06-05 15:55:17 -0400

    Figuring Out Modifiers

--- Models/Character.cs
+++ Models/Character.cs
@@ -16,17 +16,17 @@ namespace PathfinderCharacter.Models
         //public Race Race { get; set; }
         //public Size Size { get; set; }
         public string Gender { get; set; }
-        public int Age { get; set; }
-        public int Weight { get; set; }
-        public int Height { get; set; }
+        public CharacterStat Age { get; set; }
+        public CharacterStat Weight { get; set; }
+        public CharacterStat Height { get; set; }
         public string Hair { get; set; }
         public string Eyes { get; set; }
-        public int FlySpeed { get; set; }
-        public int WalkSpeed { get; set; }
-        public int SwimSpeed { get; set; }
-        public int ClimbSpeed { get; set; }
-        public int NurrowSpeed { get; set; }
-        public int MaxHitpoints { get; set; }
+        public CharacterStat FlySpeed { get; set; }
+        public CharacterStat WalkSpeed { get; set; }
+        public CharacterStat SwimSpeed { get; set; }
+        public CharacterStat ClimbSpeed { get; set; }
+        public CharacterStat BurrowSpeed { get; set; }
+        public CharacterStat MaxHitpoints { get; set; }
 
         public List<CharacterSkill> CharacterSkills {get;set;}
         public List<CharacterBloodline> CharacterBloodlines { get; set; }
@@ -36,11 +36,19 @@ namespace PathfinderCharacter.Models
         public List<Spell> CharacterSpellbook { get; set; }
     }
 
+    public class CharacterStat
+    {
+        public Guid Id { get; set; }
+        public int Value { get; set; }     
+    }
+
     public class Modifier
     {
         public Guid Id { get; set; }
         public Guid IdToModify { get; set; }
         public int Value { get; set; }
+        public string ModifierType { get; set; }
+        public bool Stackable { get; set; }
     }
 
     public class CharacterSkill
